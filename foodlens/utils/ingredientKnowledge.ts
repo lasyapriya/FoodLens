f88ca9b856361b4f7ca9c44afde84f923246c500
lasyapriya <csedm9@gmail.com>
2026-03-08 22:23:42 +0530

    Update ingredientKnowledge.ts

--- foodlens/utils/ingredientKnowledge.ts
+++ foodlens/utils/ingredientKnowledge.ts
@@ -1,1 +1,27 @@
+export const allergenMap: Record<string,string> = {
 
+  casein:"milk",
+  whey:"milk",
+  lactose:"milk",
+  lactalbumin:"milk",
+
+  albumin:"egg",
+  ovalbumin:"egg",
+
+  soy:"soy",
+  lecithin:"soy",
+
+  wheat:"wheat",
+  gluten:"wheat",
+  semolina:"wheat",
+
+  papaya:"papaya",
+  papain:"papaya",
+
+  peanut:"peanut",
+
+  almond:"tree nut",
+  cashew:"tree nut",
+  walnut:"tree nut",
+
+};
