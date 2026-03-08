ca3cc9546b3b4d38453289d7f26a843afbf41a19
lasyapriya <csedm9@gmail.com>
2026-03-08 22:25:02 +0530

    Create additiveDB.ts

---
+++ foodlens/utils/additiveDB.ts
@@ -0,0 +1,11 @@
+export const additiveDB: Record<string,string> = {
+
+  e100:"curcumin color",
+  e200:"sorbic acid preservative",
+  e211:"sodium benzoate preservative",
+  e322:"lecithin emulsifier",
+  e330:"citric acid acidity regulator",
+  e471:"mono and diglycerides emulsifier",
+  e621:"monosodium glutamate flavor enhancer"
+
+};
